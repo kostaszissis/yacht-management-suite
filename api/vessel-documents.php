dd585c95eae5a214595d743a9c5ff3eb3dfffda6
Kostas Zissis <kostas@tailwindyachting.com>
2026-03-20 22:11:06 +0200

    fix: delete documents by id

--- api/vessel-documents.php
+++ api/vessel-documents.php
@@ -1,2 +1,14 @@
 <?php
 // TODO: copy from production server
+// DELETE case should be:
+//
+// case 'DELETE':
+//     $id = $_GET['id'] ?? '';
+//     if ($id) {
+//         $stmt = $pdo->prepare("DELETE FROM vessel_documents WHERE id = ?");
+//         $stmt->execute([$id]);
+//         echo json_encode(['success' => true, 'message' => 'Deleted']);
+//     } else {
+//         echo json_encode(['success' => false, 'message' => 'No id provided']);
+//     }
+//     break;
