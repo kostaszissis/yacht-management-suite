0c487a4b1932edbdd9d9e3ecad9c6b1ff6bea9c3
Kostas Zissis <kostas@tailwindyachting.com>
2026-04-21 19:29:38 +0300

    v4.10: Fix footer version string in HomePage.tsx

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const BUILD_VERSION = 'mo8tsa9x';
-const BUILD_DATE = '2026-04-21T16:15:44.133Z';
+const BUILD_VERSION = 'mo8u6wn1';
+const BUILD_DATE = '2026-04-21T16:27:06.301Z';
 
 self.addEventListener('install', function(e) {
   self.skipWaiting();
